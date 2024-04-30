8675c4898cd54468d36d99c083a9b80ec2f0dcf3
mussyaroslav <mussyaroslav@gmail.com>
2024-04-30 12:58:39 +0500

    Вынес в отдельную функцию и сделал точнее вычисление даты

--- main.go
+++ main.go
@@ -6,37 +6,41 @@ import (
 )
 
 func main() {
-	var birthdayDate string
+	birthdayDate := birthday()
+
+	// Получаем текущую дату
+	tNow := time.Now()
+	fmt.Println("The current date: ", tNow.Format("02.01.2006"))
+
+	// Вычисляем возраст
+	age := tNow.Year() - birthdayDate.Year()
+
+	// Проверяем, прошел ли день рождения в этом году
+	if tNow.Month() < birthdayDate.Month() || (tNow.Month() == birthdayDate.Month() && tNow.Day() < birthdayDate.Day()) {
+		age--
+	}
+
+	fmt.Println("Your age: ", age)
+}
+
+func birthday() time.Time {
+	var birthdayStr string
+
 	for {
 		fmt.Print("Enter your birthday in the format dd.mm.yyyy: ")
-		_, err := fmt.Scanln(&birthdayDate)
+		_, err := fmt.Scan(&birthdayStr)
 		if err != nil {
 			fmt.Println("Input error:", err)
 			continue
 		}
 		// Предполагаем, что вводится формат "dd.mm.yy"
 		const format = "02.01.2006"
-		t, err := time.Parse(format, birthdayDate)
+		t, err := time.Parse(format, birthdayStr)
 		if err != nil {
 			fmt.Println("Incorrect date format. Please enter the date again.")
 			continue
 		}
 		fmt.Println("Your birthday: ", t.Format("02.01.2006"))
-
-		// Получаем текущую дату
-		tNow := time.Now()
-		fmt.Println("The current date: ", tNow.Format("02.01.2006"))
-
-		// Вычисляем возраст
-		age := tNow.Year() - t.Year()
-
-		// Проверяем, прошел ли день рождения в этом году
-		if tNow.YearDay() < t.YearDay() {
-			age--
-		}
-
-		fmt.Println("Your age: ", age)
-
-		break
+		return t
 	}
 }
